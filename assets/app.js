9e5a2f391aeb7b575b3d52804b0c9df51c9d9695
Kaung Htet Zaw <kaunghtetzaw912@gmail.com>
2022-08-07 23:02:50 +0200

    Libraries changes

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing-1",{
+        strings: [
+            "Engineer", "Designer", "Maker"
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
